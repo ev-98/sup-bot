f9841a0aaed8d9e7c1ea6fcbf02d19522bf33e54
E.V. Phillips <ercephillips@ufl.edu>
2020-08-28 17:38:05 -0400

    finish bot

--- README.md
+++ README.md
@@ -1,9 +1,34 @@
 # sup-bot
 Automated Supreme checkout bot
+**CURRENTLY FUNCTIONAL ON CHROME ONLY, GECKODRIVER REJECTS CAPTCHA**
+
+## How to setup
+* Install chromedriver according to your version of chrome and add it to your PATH
+> AppData/Local/Programs/Python
+* Configure your info dictionary at the bottom of the file
+* Run in terminal
+
+## Product name/ color/ size
+* Must be written with exact capitalization, no extra spacing and include special characters
+* Consult a droplist site prior to use to ensure your configurations are 100% accurate
+
+## Categories
+* jackets
+* shirts
+* tops_sweaters
+* sweatshirts
+* t-shirts
+* hats
+* bags
+* accessories
+* skate
+
+## Important note
+* Certain input names (credit card number, etc) seem random, which could mean that Supreme updates these frequently to dissuade botting. Inspecting page elements before the drop is the best way to ensure your info is input ahead of time.
 
 ## Final implementation goals
 - [ ] refreshes product site close to drop time until available
-- [ ] automatically opens requested product page
-- [ ] adds product to cart
-- [ ] redirects to checkout page
-- [ ] passes inputs into request fields, awaits checkout click
+- [ X ] automatically opens requested product page
+- [ X ] adds product to cart
+- [ X ] redirects to checkout page
+- [ X ] passes inputs into request fields, awaits checkout click
--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import requests
 import bs4
 from splinter import Browser
 
+#init bot
 class SupremeBot:
     def __init__(self, **info):
         self.base = "https://www.supremenewyork.com/"
@@ -29,24 +30,58 @@ class SupremeBot:
         self.final_link = list(set([x for x in temp_link if temp_link.count(x) == 2]))[0]
         print(self.final_link)
 
+    def visit_site(self):
+        self.b.visit("{}{}".format(self.base, str(self.final_link)))
+
+        self.b.find_option_by_text(self.info['size']).click()
+        self.b.find_by_value('add to cart').click()
+
+    def checkout_fn(self):
+        self.b.visit("{}{}".format(self.base, self.checkout))
+        self.b.visit("{}{}".format(self.base, self.checkout))
+       
+        self.b.fill("order[billing_name]", self.info['namefield'])
+        self.b.fill("order[email]", self.info['emailfield'])
+        self.b.fill("order[tel]", self.info['phonefield'])
+        self.b.fill("order[billing_address]", self.info['addressfield'])
+        self.b.fill("order[billing_zip]", self.info['zip'])
+
+        #zip autofills this, may vary based on country
+        # self.b.fill("order[billing_city]", self.info['city'])
+        # self.b.select("order[billing_state]", self.info['state'])
+        # self.b.select("order[billing_country]", self.info['country'])
+
+        #see readme, this form id seems random and subject to change
+        self.b.fill("riearmxa", self.info['number'])
+        self.b.select("credit_card[month]", self.info['month'])
+        self.b.select("credit_card[year]", self.info['year'])
+        self.b.fill("credit_card[meknk]", self.info['ccv'])
+
+        #throws an ElementClickInterceptedException, will just have to click the terms button manually
+        # self.b.find_by_name('order[terms]').click()
+
 if __name__=="__main__":
     INFO={
-    "product": "Blocks Hooded Sweatshirt",
-    "color": "Navy",
-    "size": "Medium",
-    "category": "sweatshirts",
-    "namefield": "example",
+    "product": "Lovers Tee",
+    "color": "Woodland Camo",
+    "size": "Large",
+    "category": "t-shirts",
+    "namefield": "example example",
     "emailfield": "example@example.com",
     "phonefield": "5555555555",
     "addressfield": "123 Example Dr",
-    "city": "Example",
     "zip": "55555",
-    "country": "US",
-    "card": "visa",
-    "number": "XXXXXXXXXXXXXXXX",
-    "month": "XX",
-    "year": "XXXX",
-    "ccv": "XXX",
+    # zip autofills this
+    # "city": "Example",
+    # "state": "CA",
+    # "country": "USA",
+    "number": "123456789123456789",
+    "month": "01",
+    "year": "2020",
+    "ccv": "123",
     }
     bot = SupremeBot(**INFO)
     bot.find_product()
+    bot.init_browser()
+    bot.visit_site()
+    bot.checkout_fn()
